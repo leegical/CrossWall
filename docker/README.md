6e2cd3d377e1fc8cabb9723469c0f5707b2e55f3
Bric-R <chuxuange@gmail.com>
2023-07-18 00:41:24 +0800

    一键部署docker

--- docker/README.md
+++ docker/README.md
@@ -8,24 +8,24 @@
 ## 准备工作
 以Ubuntu22.04 LTS为例
 1. 确保服务器有docker环境。如果没有，则在当前文件夹中启动终端，执行以下命令安装：
-```bash
+```shell
 sudo bash -c bash
 chmod +x install-docker.sh
 ./install-docker.sh
 ```
 
 2. 防火墙放行端口
-```bash
+```shell
 sudo ufw allow 3000:3005/tcp
 suod ufw allow 3000:3005/udp
 sduo ufw reload
 ```
 
-3. 替换OpenAI 的API Key
-将当前文件夹中`build.sh`文件、各个子文件夹中`docker-compose.yml`文件里的`OPENAI_API_KEY`都替换成你自己的
+3. 准备好你自己的[OpenAI API Key](https://platform.openai.com/account/api-keys)，运行一键安装脚本`build.sh`时会提示你输入
 
 ## 部署
 在当前文件夹中开启终端，执行以下命令，一键安装这三个项目的docker
+> 脚本执行过程中，会提示你输入自己的OpenAI API Key
 ```shell
 sudo bash -c bash
 chmod +x build.sh
--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,48 @@
 #!/bin/bash
 
+# 定义函数
+prompt_api_key() {
+    while true; do
+        read -p "请输入您的OpenAI API key（输入n或N退出）: " api_key
+        if [[ "$api_key" =~ ^[nN]$ ]]; then
+            echo "已退出脚本！"
+            exit 0
+        elif [[ "$api_key" =~ ^sk- && ${#api_key} -gt 20 ]]; then
+            export API_KEY="$api_key"
+            echo "已设置OpenAI API key为: $API_KEY"
+            echo "将使用 $API_KEY 构建服务......"
+            break
+        else
+            echo "输入的API key不合法，请重新输入: "
+        fi
+    done
+}
+
+# 调用函数
+prompt_api_key
+
+# 定义函数：为当前服务设置新的API key
+set_api_key() {
+    read -p "为当前服务设置新的API key（回车跳过设置）: " new_api_key
+    if [[ "$new_api_key" =~ ^sk- && ${#new_api_key} -gt 20 ]]; then
+        export API_KEY="$new_api_key"
+        echo "已设置当前服务API key为: $API_KEY"
+    elif [[ -z "$new_api_key" ]]; then
+        echo "未设置新的API key，当前服务将继续使用: $API_KEY"
+    else
+        echo "API key不合法，当前服务将继续使用: $API_KEY"
+    fi
+}
+
+isolate_set=0
+# 主逻辑
+read -p "是否为每个服务单独设置API key？（输入y或Y进入单独设置模式）: " isolate
+if [[ "$isolate" =~ ^[yY]$ ]]; then
+    isolate_set=1
+else
+    echo "[单一API模式]所有用户将使用全局API密钥: $API_KEY"
+fi
+
 # proj_dir=$(pwd)
 docker rm -f chat-web
 docker rm -f next-web
@@ -19,13 +62,18 @@ directories=$(find . -maxdepth 1 -type d)
 # Loop through each directory and execute docker-compose up
 for dir in $directories; do
     if [ "$dir" != "." ]; then
-        echo "Entering $dir"
         cd $dir
-        docker-compose up -d
         # 查找docker-compose.yml文件中的本机端口，并只保留第一行结果
         sublocalport=$(grep -Po '\d+:300\d' docker-compose.yml | grep -oE '[0-9]+' | head -n 1)
         # 获取当前文件夹的名称
         subdirname=$(basename "$(pwd)")
+        echo "进入 $(pwd)，构建 $subdirname 服务......"
+        if [[ "$isolate_set" -eq 1 ]]; then
+            # 为当前服务设置单独的API
+            set_api_key
+        fi
+        sed -i "s/API_KEY: .*/API_KEY: \"$API_KEY\"/g" docker-compose.yml
+        docker-compose up -d
         # 打印服务信息
         echo "请访问 $localip:$sublocalport 来使用 $subdirname 服务"
         cd ..
